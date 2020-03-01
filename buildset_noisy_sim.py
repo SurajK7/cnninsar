91c5a1254f49cbe70e382541fc598f9b84480a8e
Suraj Kumar <surajk732000@gmail.com>
2020-03-01 06:34:24 +0000

    added and modified simulator.py

--- buildset_noisy_sim.py
+++ buildset_noisy_sim.py
@@ -8,7 +8,8 @@ Created on Fri Aug 02 12:30:04 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
-from simulator import sim_coh
+from simulator import generate_sim
+from simulator import coherence
 from os import path
 
 
@@ -18,7 +19,8 @@ ifgsize = [1000,1000]
 for loop_idx in range(0, sim_cnt):
     noisy_fname = 'simtdset/noisy_' + str(loop_idx) + '.npy'
     if not path.exists(noisy_fname):
-        sim, sim_cohs = sim_coh(width=ifgsize[1],height=ifgsize[0])
+        sim = generate_sim(width=ifgsize[1],height=ifgsize[0])
+        sim_cohs = coherence(sim)
         np.save(noisy_fname,sim.ifg_noisy)
     
     print('Finished saving simulated interferogram #' + str(loop_idx+1))
